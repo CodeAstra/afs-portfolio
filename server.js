45173279d4d73b706fcc4712a336e16ea1e7de6d
BV Satyaram <bvsatyaram@gmail.com>
2020-09-16 05:43:50 +0530

    Setup livereload

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const express = require("express");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
 
 const app = express();
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'), path.join(__dirname, 'views'));
 
 app.use(express.static("public"));
+app.use(connectLivereload());
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
